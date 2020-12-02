f866561fb7cf76d9cfa03b5705580c78fccac4a4
Quinn Price <quinn@strongsalt.com>
2020-12-02 10:21:28 -0800

    partial e2ee UploadDoc

--- api/doc-e2ee.go
+++ api/doc-e2ee.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/base64"
 	// "bytes"
 	"context"
 	"fmt"
@@ -8,133 +9,262 @@ import (
 
 	"github.com/overnest/strongdoc-go-sdk/client"
 	"github.com/overnest/strongdoc-go-sdk/proto"
-	// ssc "github.com/overnest/strongsalt-crypto-go"
+	ssc "github.com/overnest/strongsalt-crypto-go"
 	// "github.com/overnest/strongdoc-go-sdk/utils"
 )
 
-type EncryptedKey struct{
-	encKey string
-   	encryptorID string
-    encryptorVersion int32
-   	keyID string
-    keyVersion int32
-   	ownerID string
+type EncryptedKey struct {
+	encKey           string
+	encryptorID      string
+	encryptorVersion int32
+	keyID            string
+	keyVersion       int32
+	ownerID          string
 }
 
 // UploadDocument uploads a document to Strongdoc-provided storage.
 // It then returns a docId that uniquely identifies the document.
-func E2EEUploadDocument(docName string, plainStream io.Reader) (docID string, err error) {
-	// sdc, err := client.GetStrongDocClient()
-	// if err != nil {
-	// 	return
-	// }
-	// sdm, err := client.GetStrongDocManager()
-	// if err != nil {
-	// 	return
-	// }
-	// passwordKey := sdm.GetPasswordKey()
+/*func E2EEUploadDocument_Old(docName string, plainStream io.Reader) (docID string, err error) {
+	sdc, err := client.GetStrongDocClient()
+	if err != nil {
+		return
+	}
 
-	// stream, err := sdc.E2EEUploadDocumentStream(context.Background())
-	// if err != nil {
-	// 	err = fmt.Errorf("UploadDocument err: [%v]", err)
-	// 	return
-	// }
+	passwordKey := sdc.GetPasswordKey()
 
-	// preReq := &proto.E2EEUploadDocStreamReq{
-	// 	StagesOfUploading: &proto.E2EEUploadDocStreamReq_PreMetaData_ {
-	// 		PreMetaData: &proto.E2EEUploadDocStreamReq_PreMetaData{
-	// 			DocName: docName,
-	// 			// IsEncryptedByClient: true,
-	// 		},
-	// 	},
-	// }
+	stream, err := sdc.GetGrpcClient().E2EEUploadDocumentStream(context.Background())
+	if err != nil {
+		err = fmt.Errorf("UploadDocument err: [%v]", err)
+		return
+	}
 
-	// err = stream.Send(preReq)
-	// if err != nil {
-	// 	err = fmt.Errorf("UploadDocument stage PreMetaData err: [%v]", err)
-	// 	return
-	// }
+	preReq := &proto.E2EEUploadDocStreamReq{
+		StagesOfUploading: &proto.E2EEUploadDocStreamReq_PreMetaData_{
+			PreMetaData: &proto.E2EEUploadDocStreamReq_PreMetaData{
+				DocName: docName,
+				// IsEncryptedByClient: true,
+			},
+		},
+	}
 
-	// userPrivKeysReq := &proto.GetUserPrivateKeysReq{}
-	// userPrivKeysResp, err := sdc.GetUserPrivateKeys(context.Background(), userPrivKeysReq)
-	// if err != nil {
-	// 	err = fmt.Errorf("UploadDocument cannot get user private keys")
-	// 	return
-	// }
-	// userPrivKeys := userPrivKeysResp.GetEncryptedKeys()
-	// docKey, err := ssc.GenerateKey(ssc.Type_Secretbox)
-	// if err != nil {
-	// 	return "", fmt.Errorf("UploadDocument cannot generate document key")
-	// }
-	// docKeySerialized, err :=  docKey.Serialize();
-	// if err != nil {
-	// 	err = fmt.Errorf("UploadDocument cannot serialize document key")
-	// 	return
-	// }
+	err = stream.Send(preReq)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument stage PreMetaData err: [%v]", err)
+		return
+	}
 
-	// encKeyListReq := &proto.E2EEUploadDocStreamReq{
-	// 	StagesOfUploading: &proto.E2EEUploadDocStreamReq_EncKeyList {
-	// 		EncDocKeys: &proto.E2EEUploadDocStreamReq_EncKeyList{
-	// 			EncDocKeys: make(*proto.EncryptedKey, 3),
-	// 		},
-	// 	},
-	// }
+	userPrivKeysReq := &proto.GetUserPrivateKeysReq{}
+	userPrivKeysResp, err := sdc.GetGrpcClient().GetUserPrivateKeys(context.Background(), userPrivKeysReq)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument cannot get user private keys")
+		return
+	}
+	userPrivKeys := userPrivKeysResp.GetEncryptedKeys()
+	docKey, err := ssc.GenerateKey(ssc.Type_Secretbox)
+	if err != nil {
+		return "", fmt.Errorf("UploadDocument cannot generate document key")
+	}
+	docKeySerialized, err := docKey.Serialize()
+	if err != nil {
+		err = fmt.Errorf("UploadDocument cannot serialize document key")
+		return
+	}
 
-	// for _, userPrivKey := range userPrivKeys{
+	encKeyListReq := &proto.E2EEUploadDocStreamReq{
+		StagesOfUploading: &proto.E2EEUploadDocStreamReq_EncDocKeys{
+			EncDocKeys: &proto.E2EEUploadDocStreamReq_EncKeyList{
+				EncDocKeys: make([]*proto.EncryptedKey, 3),
+			},
+		},
+	}
+
+	for _, userPrivKey := range userPrivKeys {
 		// TODO: Check user password key version and id
-		// decryptedUserPrivKey, err := passwordKey.Decrypt([]byte(userPrivKey.GetEncKey()))
-		// if err != nil {
-		// 	return "", fmt.Errorf("UploadDocument cannot decrypt user private key")
-		// }
-		// userPrivKey, err := ssc.DeserializeKey(decryptedUserPrivKey)
-		// if err != nil {
-		// 	return "", fmt.Errorf("UploadDocument cannot deserialize user private key")
-		// }
-		// encryptedDocKey, err := userPrivKey.Encrypt(docKeySerialized)
-		// if err != nil {
-		// 	return "", fmt.Errorf("UploadDocument cannot encrypt document key")
-		// }
-	// }
+		decryptedUserPrivKey, err := passwordKey.Decrypt([]byte(userPrivKey.GetEncKey()))
+		if err != nil {
+			return "", fmt.Errorf("UploadDocument cannot decrypt user private key")
+		}
+		userPrivKey, err := ssc.DeserializeKey(decryptedUserPrivKey)
+		if err != nil {
+			return "", fmt.Errorf("UploadDocument cannot deserialize user private key")
+		}
+		encryptedDocKey, err := userPrivKey.Encrypt(docKeySerialized)
+		if err != nil {
+			return "", fmt.Errorf("UploadDocument cannot encrypt document key")
+		}
+	}
 
+	block := make([]byte, blockSize)
+	for {
+		n, inerr := plainStream.Read(block)
+		if inerr == nil || inerr == io.EOF {
+			if n > 0 {
+				dataReq := &proto.E2EEUploadDocStreamReq{
+					StagesOfUploading: &proto.E2EEUploadDocStreamReq_CipherText{
+						//TODO: need encryption
+						CipherText: block[:n]}}
+				if err = stream.Send(dataReq); err != nil {
+					return "", err
+				}
+			}
+			if inerr == io.EOF {
+				break
+			}
+		} else {
+			err = inerr
+			return
+		}
+	}
 
-	// block := make([]byte, blockSize)
-	// for {
-	// 	n, inerr := plainStream.Read(block)
-	// 	if inerr == nil || inerr == io.EOF {
-	// 		if n > 0 {
-	// 			dataReq := &proto.E2EEUploadDocStreamReq{
-	// 				StagesOfUploading: &proto.E2EEUploadDocStreamReq_CipherText {
-	// 					//TODO: need encryption
-	// 					CipherText: block[:n]}}
-	// 			if err = stream.Send(dataReq); err != nil {
-	// 				return "", err
-	// 			}
-	// 		}
-	// 		if inerr == io.EOF {
-	// 			break
-	// 		}
-	// 	} else {
-	// 		err = inerr
-	// 		return
-	// 	}
-	// }
+	if err = stream.CloseSend(); err != nil {
+		err = fmt.Errorf("UploadDocument err: [%v]", err)
+		return
+	}
 
-	// if err = stream.CloseSend(); err != nil {
-	// 	err = fmt.Errorf("UploadDocument err: [%v]", err)
-	// 	return
-	// }
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		err = fmt.Errorf("UploadDocument err: [%v]", err)
+		return
+	}
 
-	// resp, err := stream.CloseAndRecv()
-	// if err != nil {
-	// 	err = fmt.Errorf("UploadDocument err: [%v]", err)
-	// 	return
-	// }
+	if resp == nil {
+		err = fmt.Errorf("UploadDocument Resp is nil: [%v]", err)
+		return
+	}
 
-	// if resp == nil {
-	// 	err = fmt.Errorf("UploadDocument Resp is nil: [%v]", err)
-	// 	return
-	// }
+	docID = resp.GetDocID()
+	return
+}*/
 
-	// docID = resp.GetDocID()
+// UploadDocument uploads a document to Strongdoc-provided storage.
+// It then returns a docId that uniquely identifies the document.
+func E2EEUploadDocument(sdc client.StrongDocClient, docName string, plainStream io.Reader) (docID string, err error) {
+	docKey, err := ssc.GenerateKey(ssc.Type_XChaCha20HMAC)
+	if err != nil {
+		return "", fmt.Errorf("UploadDocument cannot generate document key")
+	}
+	docKeySerialized, err := docKey.Serialize()
+	if err != nil {
+		err = fmt.Errorf("UploadDocument cannot serialize document key")
+		return
+	}
+
+	stream, err := sdc.GetGrpcClient().E2EEUploadDocumentStream(context.Background())
+	defer stream.CloseSend()
+
+	if err != nil {
+		err = fmt.Errorf("UploadDocument err: [%v]", err)
+		return
+	}
+
+	preReq := &proto.E2EEUploadDocStreamReq{
+		StageData: &proto.E2EEUploadDocStreamReq_PreMetaData{
+			PreMetaData: &proto.E2EEUploadDocStreamReq_PreMetaDataType{
+				DocName: docName,
+			},
+		},
+	}
+
+	err = stream.Send(preReq)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument stage PreMetaData err: [%v]", err)
+		return
+	}
+
+	resp, err := stream.Recv()
+
+	for !resp.GetReadyForData() {
+		protoPubKeys := resp.GetEncryptors().GetPubKeys()
+		protoEncDocKeys := make([]*proto.EncryptedKey, len(protoPubKeys))
+		for i, protoPubKey := range protoPubKeys {
+			keyBytes, err := base64.URLEncoding.DecodeString(protoPubKey.GetKey())
+			if err != nil {
+				return "", fmt.Errorf("UploadDocument error base64 decoding public key string: [%v]", err)
+			}
+
+			key, err := ssc.DeserializeKey(keyBytes)
+			if err != nil {
+				return "", fmt.Errorf("UploadDocument error deserializing public key bytes: [%v]", err)
+			}
+
+			encDocKey, err := key.Encrypt(docKeySerialized)
+			if err != nil {
+				return "", fmt.Errorf("UploadDocument error encrypting document key: [%v]", err)
+			}
+			encDocKeyBase64 := base64.URLEncoding.EncodeToString(encDocKey)
+
+			encKey := &proto.EncryptedKey{
+				EncryptorID:      protoPubKey.GetKeyID(),
+				OwnerID:          protoPubKey.GetOwnerID(),
+				OwnerType:        protoPubKey.GetOwnerType(),
+				EncryptorVersion: protoPubKey.GetVersion(),
+				EncKey:           encDocKeyBase64,
+			}
+
+			protoEncDocKeys[i] = encKey
+		}
+
+		req := &proto.E2EEUploadDocStreamReq{
+			StageData: &proto.E2EEUploadDocStreamReq_EncDocKeys{
+				EncDocKeys: &proto.E2EEUploadDocStreamReq_EncKeyList{
+					EncDocKeys: protoEncDocKeys,
+				},
+			},
+		}
+		err = stream.Send(req)
+		if err != nil {
+			return "", fmt.Errorf("UploadDocument error sending encrypted document keys: [%v]", err)
+		}
+
+		resp, err = stream.Recv()
+		if err != nil {
+			return "", fmt.Errorf("UploadDocument error receiving response after sending document keys: [%v]", err)
+		}
+	}
+
+	req := &proto.E2EEUploadDocStreamReq{
+		StageData: &proto.E2EEUploadDocStreamReq_CipherText{
+			CipherText: []byte("TODO: actual data"),
+		},
+	}
+
+	err = stream.Send(req)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument error sending ciphertext: [%v]", err)
+		return
+	}
+
+	_, err = docKey.MACWrite([]byte("TODO: actual data"))
+	if err != nil {
+		return "", fmt.Errorf("UploadDocument error writing ciphertext to MAC: [%v]", err)
+	}
+
+	docMAC, err := docKey.MACSum(nil)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument error reading final MAC: [%v]", err)
+		return
+	}
+
+	req = &proto.E2EEUploadDocStreamReq{
+		StageData: &proto.E2EEUploadDocStreamReq_PostMetaData{
+			PostMetaData: &proto.E2EEUploadDocStreamReq_PostMetaDataType{
+				MacOfCipherText: base64.URLEncoding.EncodeToString(docMAC),
+			},
+		},
+	}
+
+	err = stream.Send(req)
+	if err != nil {
+		err = fmt.Errorf("UploadDocument error sending PostMetaData: [%v]", err)
+		return
+	}
+
+	resp, err = stream.Recv()
+	if err != nil {
+		err = fmt.Errorf("UploadDocument error receiving DocID: [%v]", err)
+	}
+	docID = resp.GetDocID()
+
+	stream.CloseSend()
 	return
 }
--- test/e2ee_doc_test.go
+++ test/e2ee_doc_test.go
@@ -2,41 +2,44 @@ package test
 
 import (
 	"fmt"
+
 	"github.com/overnest/strongdoc-go-sdk/api"
 	"github.com/overnest/strongdoc-go-sdk/client"
+
 	// cryptoKey "github.com/overnest/strongsalt-crypto-go"
 	// cryptoKdf "github.com/overnest/strongsalt-crypto-go/kdf"
 	// "path"
-	"testing"
 	"os"
+	"testing"
+
 	// "io/ioutil"
 	"gotest.tools/assert"
 )
 
-func initTest(t *testing.T) string{
+/*func initTest(t *testing.T) string {
 	// init client
-	_, err := client.InitStrongDocManager(client.LOCAL, false)
+	sdc, err := client.InitStrongDocClient(client.LOCAL, false)
 	assert.NilError(t, err)
 
 	// register org and admin
 	orgs, orgUsers := initData(1, 1)
 	orgData := orgs[0]
 	userData := orgUsers[0][0]
-	err = registerOrgAndAdmin(orgData, userData)
+	err = registerOrgAndAdmin(sdc, orgData, userData)
 	assert.NilError(t, err)
 	// defer HardRemoveOrgs([]string{orgData.OrgID})
-	
+
 	// login
-	adminToken, err := api.Login(userData.UserID, userData.Password, orgData.OrgID, userData.PasswordKeyPwd)
+	adminToken, err := sdc.Login(userData.UserID, userData.Password, orgData.OrgID, userData.PasswordKeyPwd)
 	assert.NilError(t, err)
 	return adminToken
-}
+}*/
 
 // func TestUploadDownloadE2EE {
 
 // }
 
-func testE2EEUpload(t *testing.T) {
+func testE2EEUpload(t *testing.T, sdc client.StrongDocClient) {
 	// txtBytes, err := ioutil.ReadFile(fileName)
 	// assert.NilError(t, err)
 
@@ -44,20 +47,20 @@ func testE2EEUpload(t *testing.T) {
 	assert.NilError(t, err)
 	defer file.Close()
 
-	uploadDocID, err := api.E2EEUploadDocument("TestDoc1", file)
-	// assert.Equal(t, err)
+	uploadDocID, err := api.E2EEUploadDocument(sdc, "TestDoc1", file)
+	assert.NilError(t, err)
 	fmt.Println(uploadDocID)
 
 	// downBytes, err := api.E2EEDownloadDocument(uploadDocID)
 	// assert.NilError(t, err)
 	// assert.Equal(t, txtBytes, downBytes)
 
-	// docs, err := api.ListDocuments()
-	// assert.NilError(t, err)
-	// assert.Equal(t, len(docs), 1)
+	docs, err := api.ListDocuments(sdc)
+	assert.NilError(t, err)
+	assert.Equal(t, len(docs), 1)
 
-	// err = api.RemoveDocument(uploadDocID)
-	// assert.NoError(t, err)
+	err = api.RemoveDocument(sdc, uploadDocID)
+	assert.NilError(t, err)
 
 	// docs, err = api.ListDocuments()
 	// assert.NoError(t, err)
@@ -71,17 +74,17 @@ func testE2EEUpload(t *testing.T) {
 	// defer file.Close()
 }
 
-func TestE2EEUpload(t *testing.T){
-	_, registeredOrgUsers, orgids, err := testSetup(1, 1)
+func TestE2EEUpload(t *testing.T) {
+	sdc, _, registeredOrgUsers, orgids, err := testSetup(1, 1)
 	assert.NilError(t, err)
 	defer testTeardown(orgids)
 	t.Run("test e2ee upload", func(t *testing.T) {
 		admin := registeredOrgUsers[0][0]
 		// admin login
-		_, err := api.Login(admin.UserID, admin.Password, admin.OrgID, admin.PasswordKeyPwd)
+		_, err := api.Login(sdc, admin.UserID, admin.Password, admin.OrgID, admin.PasswordKeyPwd)
 		assert.NilError(t, err)
-		defer api.Logout()
-		testE2EEUpload(t)
+		defer api.Logout(sdc)
+		testE2EEUpload(t, sdc)
 	})
 
 }
